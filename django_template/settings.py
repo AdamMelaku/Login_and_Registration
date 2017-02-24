57cf9bc6b5ac27961da9643155993ce34df26824
Adam Melaku <AdamMelaku362@gmail.com>
2017-02-24 13:25:03 -0600

    new application running

--- django_template/settings.py
+++ django_template/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.NinjaGold',
+    'apps.main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
